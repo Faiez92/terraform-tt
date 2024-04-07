4d0cb8c560fc95bf1ab9ece513b33dca4dfc79c1
faiez <faiez162@gmail.com>
2024-04-07 23:56:31 +0200

    fix cidr_block error

--- security_group.tf
+++ security_group.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "alb" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks = [data.aws_vpc.shared.cidr_block]
     ipv6_cidr_blocks = ["::/0"]
   }
 
