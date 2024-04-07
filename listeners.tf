30f7102841100111804fb9a2653b5957e00c4aeb
faiez <faiez162@gmail.com>
2024-04-07 04:29:30 +0200

    Upgrade ELB SSL policy to '17-08' for security improvement

--- listeners.tf
+++ listeners.tf
@@ -2,7 +2,7 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.main.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  ssl_policy        = "ELBSecurityPolicy-2017-08"
   certificate_arn   = aws_acm_certificate_validation.shared.certificate_arn
 
   default_action {
