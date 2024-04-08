b5c26e55cd8efac7a6330ae6d31724c7eef79cbe
faiez <faiez162@gmail.com>
2024-04-08 02:47:17 +0200

    optimise certificates.tf code

--- gitlab_certificates.tf
+++ gitlab_certificates.tf
@@ -1,16 +1,19 @@
+# Define local variables for better readability
+locals {
+  gitlab_domain_name = data.aws_route53_zone.gitlab.name
+}
+
+# Create ACM certificate resource
 resource "aws_acm_certificate" "gitlab" {
-  domain_name       = data.aws_route53_zone.gitlab.name
+  domain_name       = local.gitlab_domain_name
   validation_method = "DNS"
 
   lifecycle {
     create_before_destroy = true
   }
-
-  tags = {
-    Purpose = "Gitlab Certificate validation" 
-  }
 }
 
+# Create Route53 records for ACM certificate validation
 resource "aws_route53_record" "gitlab_certificate_validation" {
   for_each = {
     for option in aws_acm_certificate.gitlab.domain_validation_options : option.domain_name => {
@@ -20,15 +23,22 @@ resource "aws_route53_record" "gitlab_certificate_validation" {
     }
   }
 
-  allow_overwrite = true
+  zone_id         = data.aws_route53_zone.gitlab.zone_id
   name            = each.value.name
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.gitlab.zone_id
+  allow_overwrite = true
 }
 
+# Create ACM certificate validation resource
 resource "aws_acm_certificate_validation" "gitlab" {
   certificate_arn         = aws_acm_certificate.gitlab.arn
-  validation_record_fqdns = [for record in aws_route53_record.gitlab_certificate_validation : record.fqdn]
+  validation_record_fqdns = [
+    for record in aws_route53_record.gitlab_certificate_validation : record.fqdn
+  ]
+
+  depends_on = [
+    aws_route53_record.gitlab_certificate_validation,
+  ]
 }
